06190389bd88dc77e23ac87ef4424fe118f36713
Sivakumar <sivakumarganga@gmail.com>
2018-11-23 16:50:21 +0530

    Added a basic skeleton and a login page. --WIP

---
+++ src/Feature/Login/code/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using SitecoreLogin.Feature.Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreLogin.Feature.Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Views/Feature/Login/UserLogin/LoginForm/Index.cshtml", new LoginViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult LoginUser(LoginViewModel loginViewModel)
+        {
+            if (!this.IsUserValid(loginViewModel.Email, loginViewModel.Password))
+            {
+                loginViewModel.IsInvalidAttempt = true;
+                return View("~/Views/Feature/Login/UserLogin/LoginForm/Index.cshtml", loginViewModel);
+            }
+
+            return Redirect("/dashboard");
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return View("~/Views/Feature/Login/UserLogin/ForgotPassword/Index.cshtml");
+        }
+
+        private bool IsUserValid(string email, string password)
+        {
+            return email == "suhas" && password == "suhas";
+        }
+    }
+}
---
+++ src/Feature/Login/code/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreLogin.Feature.Login.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsInvalidAttempt { get; set; }
+
+        public string LoginDate
+        {
+            get
+            {
+                return DateTime.Now.ToString();
+            }
+        }
+    }
+}
